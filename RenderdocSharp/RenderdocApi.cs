0f560ef55ff1eb85db7a9da8a212db97e8706b01
H. Utku Maden <utkumaden@hotmail.com>
2025-07-26 18:38:27 +0300

    Fix unmanaged calling conventions from Runtime default to Cdecl.

--- RenderdocSharp/RenderdocApi.cs
+++ RenderdocSharp/RenderdocApi.cs
@@ -3,49 +3,49 @@ namespace RenderdocSharp
 #pragma warning disable CS0649
     internal unsafe struct RenderdocApi
     {
-        public delegate*<int*, int*, int*, void> GetApiVersion;
-        
-        public delegate*<CaptureOption, int, int> SetCaptureOptionU32;
-        public delegate*<CaptureOption, float, int> SetCaptureOptionF32;
-        public delegate*<CaptureOption, int> GetCaptureOptionU32;
-        public delegate*<CaptureOption, float> GetCaptureOptionF32;
+        public delegate* unmanaged[Cdecl]<int*, int*, int*, void> GetApiVersion;
 
-        public delegate*<InputButton*, int, void> SetFocusToggleKeys;
-        public delegate*<InputButton*, int, void> SetCaptureKeys;
+        public delegate* unmanaged[Cdecl]<CaptureOption, int, int> SetCaptureOptionU32;
+        public delegate* unmanaged[Cdecl]<CaptureOption, float, int> SetCaptureOptionF32;
+        public delegate* unmanaged[Cdecl]<CaptureOption, int> GetCaptureOptionU32;
+        public delegate* unmanaged[Cdecl]<CaptureOption, float> GetCaptureOptionF32;
 
-        public delegate*<OverlayBits> GetOverlayBits;
-        public delegate*<OverlayBits, OverlayBits, void> MaskOverlayBits;
+        public delegate* unmanaged[Cdecl]<InputButton*, int, void> SetFocusToggleKeys;
+        public delegate* unmanaged[Cdecl]<InputButton*, int, void> SetCaptureKeys;
 
-        public delegate*<void> RemoveHooks;
-        public delegate*<void> UnloadCrashHandler;
-        public delegate*<byte*, void> SetCaptureFilePathTemplate;
-        public delegate*<byte*> GetCaptureFilePathTemplate;
+        public delegate* unmanaged[Cdecl]<OverlayBits> GetOverlayBits;
+        public delegate* unmanaged[Cdecl]<OverlayBits, OverlayBits, void> MaskOverlayBits;
 
-        public delegate*<int> GetNumCaptures;
-        public delegate*<int, byte*, int*, long*, int> GetCapture;
-        public delegate*<void> TriggerCapture;
-        public delegate*<int> IsTargetControlConnected;
-        public delegate*<int, byte*, int> LaunchReplayUI;
+        public delegate* unmanaged[Cdecl]<void> RemoveHooks;
+        public delegate* unmanaged[Cdecl]<void> UnloadCrashHandler;
+        public delegate* unmanaged[Cdecl]<byte*, void> SetCaptureFilePathTemplate;
+        public delegate* unmanaged[Cdecl]<byte*> GetCaptureFilePathTemplate;
 
-        public delegate*<void*, void*, void> SetActiveWindow;
-        public delegate*<void*, void*, void> StartFrameCapture;
-        public delegate*<int> IsFrameCapturing;
-        public delegate*<void*, void*, int> EndFrameCapture;
+        public delegate* unmanaged[Cdecl]<int> GetNumCaptures;
+        public delegate* unmanaged[Cdecl]<int, byte*, int*, long*, int> GetCapture;
+        public delegate* unmanaged[Cdecl]<void> TriggerCapture;
+        public delegate* unmanaged[Cdecl]<int> IsTargetControlConnected;
+        public delegate* unmanaged[Cdecl]<int, byte*, int> LaunchReplayUI;
+
+        public delegate* unmanaged[Cdecl]<void*, void*, void> SetActiveWindow;
+        public delegate* unmanaged[Cdecl]<void*, void*, void> StartFrameCapture;
+        public delegate* unmanaged[Cdecl]<int> IsFrameCapturing;
+        public delegate* unmanaged[Cdecl]<void*, void*, int> EndFrameCapture;
 
         // 1.1
-        public delegate*<int, void> TriggerMultiFrameCapture;
+        public delegate* unmanaged[Cdecl]<int, void> TriggerMultiFrameCapture;
 
         // 1.2
-        public delegate*<byte*, byte*, void> SetCaptureFileComments;
+        public delegate* unmanaged[Cdecl]<byte*, byte*, void> SetCaptureFileComments;
 
         // 1.3
-        public delegate*<void*, void*, int> DiscardFrameCapture;
+        public delegate* unmanaged[Cdecl]<void*, void*, int> DiscardFrameCapture;
 
         // 1.5
-        public delegate*<int> ShowReplayUI;
+        public delegate* unmanaged[Cdecl]<int> ShowReplayUI;
 
         // 1.6
-        public delegate*<byte*, void> SetCaptureTitle;
+        public delegate* unmanaged[Cdecl]<byte*, void> SetCaptureTitle;
     }
 #pragma warning restore CS0649
 }
